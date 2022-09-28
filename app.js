7953fa17823907ec75b2f2d61412fb7850a3887f
Manish <manish090801@gmail.com>
2022-09-29 00:34:04 +0530

    Adding more Files

---
+++ app.js
@@ -0,0 +1,85 @@
+//jshint esversion:6
+require("dotenv").config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require('lodash');
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb+srv://admin-manish:test-123@cluster0.yebglx5.mongodb.net/blogdb');
+
+const blogSchema = {
+  title: String,
+  content: String
+}
+
+const blog = mongoose.model("Blog", blogSchema);
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutStartingContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactStartingContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// const posts = [];
+
+app.get("/", (req, res) => {
+
+  blog.find({}, (err, posts) => {
+
+    res.render("home", { homeContent: homeStartingContent, postDec: posts });
+  })
+})
+app.get("/about", (req, res) => {
+  res.render("about", { aboutContent: aboutStartingContent });
+})
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactContent: contactStartingContent });
+})
+app.get("/compose", (req, res) => {
+  res.render("compose");
+})
+app.get("/posts/:postID", (req, res) => {
+  // const reqTitle = _.lowerCase(req.params.postName);
+  // posts.forEach((ele)=>{
+  // const postTitle = _.lowerCase(ele.title);
+  // if(postTitle === reqTitle){
+    // }
+    // })
+    blog.findOne({ _id : req.params.postID }, (err, ele) => {
+      if(!err){
+      res.render("post" , {postTitle : ele.title , postContent: ele.content})
+
+    }
+  })
+})
+
+app.post("/compose", (req, res) => {
+  // console.log(req.body.postTitle);
+  const post = new blog({
+    title: req.body.postTitle,
+    content: req.body.postDesc
+  })
+  // posts.push(post);
+  // console.log(posts);
+  post.save();
+  res.redirect("/");
+  // console.log(post);
+})
+
+
+
+
+
+
+
+
+
+app.listen( process.env.PORT ||3000, function () {
+  console.log("Server started on port 3000");
+});
